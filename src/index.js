59a8fa0ae045fabef2d114402976e1955a457c92
CioraC <cristian-sorin.ciora@haufe-lexware.com>
2018-03-01 12:37:48 +0200

    cleanup code which is not used

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import { browserHistory } from 'react-router';
 import Root from './Root'; 
 //import registerServiceWorker from './registerServiceWorker';
 
 require('./styles/overtime.css'); 
 
-//ReactDOM.render(<Root history={browserHistory} />, document.getElementById('app'));
 ReactDOM.render(<Root />, document.getElementById('root'));
 //registerServiceWorker();
